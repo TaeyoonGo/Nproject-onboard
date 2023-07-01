c2ed335d9038ee200fcd75136ac79ff77b822d3a
TaeyoonGo <taeyoongo007@gmail.com>
2023-07-01 18:38:03 +0900

    complete 작덥 완료

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -3,13 +3,13 @@ data.email = {
     title: '이메일',
     list: [
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '가입완료',
             category: 'email',
             page: '가입완료'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '임시비밀번호',
             category: 'email',
             page: '임시비밀번호'
@@ -20,43 +20,43 @@ data.login = {
     title: '로그인',
     list: [
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '가입완료_이메일 요청',
             category: 'login',
             page: '가입완료_이메일 요청'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '로그인화면',
             category: 'login',
             page: '로그인화면'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '비밀번호 재설정_비밀번호 변경',
             category: 'login',
             page: '비밀번호 재설정_비밀번호 변경'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '비밀번호 재설정_이메일_발송',
             category: 'login',
             page: '비밀번호 재설정_이메일_발송'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '비밀번호 재설정_이메일_주소',
             category: 'login',
             page: '비밀번호 재설정_이메일_주소'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '회원가입',
             category: 'login',
             page: '회원가입'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '계정설정',
             category: 'login',
             page: '계정설정'
@@ -67,7 +67,7 @@ data.join = {
     title: '가입절차',
     list: [
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '사업자번호확인',
             category: 'join',
             page: '사업자번호확인'
@@ -79,7 +79,7 @@ data.join = {
             page: '서비스정보입력'
         },
         {
-            stateClass: 'working',
+            stateClass: 'confirm',
             url: '결제수단선택',
             category: 'join',
             page: '결제수단선택'
@@ -98,19 +98,19 @@ data.join = {
         },
 
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '계약담당자정보_입력',
             category: 'join',
             page: '계약담당자정보_입력'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '국내완료',
             category: 'join',
             page: '국내완료'
         },
         {
-            stateClass: 'complete',
+            stateClass: 'confirm',
             url: '해외완료',
             category: 'join',
             page: '해외완료'
