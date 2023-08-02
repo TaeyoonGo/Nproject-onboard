c617fa01acd9baafda954249b06b35fc6a7ab6ed
gotaeyoon <ty.go.payple.kr>
2023-08-02 09:34:32 +0900

    문구수정 완료

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -325,7 +325,7 @@ data.developercenter = {
             page: '웹훅_상세정보_검색결과있음'
         },
         {
-            stateClass: 'working',
+            stateClass: 'confirm',
             url: '웹훅_상세정보_검색결과있음_라이브',
             category: 'developercenter',
             page: '웹훅_상세정보_검색결과있음_라이브'
