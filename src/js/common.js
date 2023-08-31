9c618b132bef878007b0f18c4f505c7483036eb2
gotaeyoon <ty.go.payple.kr>
2023-08-31 13:49:04 +0900

    달력 커스텀 완료

--- src/js/common.js
+++ src/js/common.js
@@ -115,7 +115,7 @@ function HoverStopService(){
 
 function checkAll() {
     $('._check_all').on('click',function(){
-        console.log('hello')
+
         // if($("#cbx_chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
         // else $("input[name=chk]").prop("checked", false);
         if($("._check_all").is(":checked")){
@@ -171,8 +171,56 @@ function asideButton(){
 //달력
 function modalCalender(){
     const parentElement = $('#datepickerParent');
-    let dateRangePicker = $('input[name=dateRangePicker]');
-    dateRangePicker.daterangepicker({
+    let twoCalender = $('._twoCalender');
+    let OneCalender = $('._oneCalender');
+    
+    
+    // 두개 보이는 달력
+    twoCalender.daterangepicker({
+        linkedCalendars: true,
+        autoApply : true,
+        "locale": {
+            "format": "YYYY-MM-DD",
+            "separator": " ~ ",
+            "applyLabel": "확인",
+            "cancelLabel": "취소",
+            "fromLabel": "From",
+            "toLabel": "To",
+            "customRangeLabel": "Custom",
+            "weekLabel": "주",
+            "daysOfWeek": ["일", "월", "화", "수", "목", "금", "토"],
+            "monthNames": ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+        },
+        "startDate": new Date(),
+        "endDate": new Date(),
+        // "minDate": new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+        // "maxDate": new Date(),
+        "buttonClasses": "data_range_btn",
+        "applyButtonClasses": "apply_range_btn",
+        "cancelButtonClasses": "cancel_range_btn",
+        "showCustomRangeLabelranges": false,
+        "parentEl": parentElement,
+    }, function (start, end, label) {
+        console.log('선택된 날짜: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+    twoCalender.on('hide.daterangepicker', function (ev, picker) {
+        let modals = $('.modal')
+        modals.removeClass('fade')
+    });
+    twoCalender.on('showCalendar.daterangepicker', function (ev, picker) {
+        const trList = $('.daterangepicker').find("tr");
+        trList.each(function() {
+            var offEndsAvailableCount = $(this).find(".off.ends.available").length;
+            if (offEndsAvailableCount === 7) {
+                $(this).hide();
+            }
+        });
+
+    });
+
+
+    //한개보이는 달력
+    OneCalender.daterangepicker({
         linkedCalendars: true,
         autoApply : true,
         "locale": {
@@ -199,11 +247,11 @@ function modalCalender(){
     }, function (start, end, label) {
         console.log('선택된 날짜: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
-    dateRangePicker.on('hide.daterangepicker', function (ev, picker) {
+    OneCalender.on('hide.daterangepicker', function (ev, picker) {
         let modals = $('.modal')
         modals.removeClass('fade')
     });
-    dateRangePicker.on('showCalendar.daterangepicker', function (ev, picker) {
+    OneCalender.on('showCalendar.daterangepicker', function (ev, picker) {
         const trList = $('.daterangepicker').find("tr");
         trList.each(function() {
             var offEndsAvailableCount = $(this).find(".off.ends.available").length;
@@ -211,8 +259,22 @@ function modalCalender(){
                 $(this).hide();
             }
         });
+    });
+    OneCalender.on('showCalendar.daterangepicker', function (ev, picker) {
+        if (picker.endDate.month() > picker.startDate.month()) {
+            // call send
+            alert('선택한 범위가 여러 달에 걸쳐 있습니다.');
+            picker.startDate = moment();
+            picker.endDate = moment();
+        }
 
     });
+
+
+
+
+
+
 }
 
 
